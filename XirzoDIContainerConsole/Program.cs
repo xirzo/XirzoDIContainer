f1b0eaae913ecf134e838b03f4214a02e0ed5331
xirzo <easttem@gmail.com>
2025-01-16 19:04:49 +0300

    chore: console test playground added

---
+++ XirzoDIContainerConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using XirzoDIContainer.Container;
+using XirzoResult;
+
+namespace XirzoDIContainer.Console
+{
+    internal class Program
+    {
+        private interface IMock
+        {
+            void Print();
+        }
+
+        private class Mock : IMock
+        {
+            public void Print()
+            {
+                System.Console.WriteLine("Hello, World!");
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            ContainerDi container = new ContainerDi();
+
+            container.Bind<Mock>(() => new Mock()).AsSingleton();
+
+            Result<Mock> result = container.Resolve<Mock>();
+        }
+    }
+}
