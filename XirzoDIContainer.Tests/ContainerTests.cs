ea231e0be5d1938b3d663e6a7ea2f1cb30e96183
Aleksandr Zhidkov <EastTem@gmail.com>
2025-01-16 17:30:09 +0300

    tests: more basic tests added

--- XirzoDIContainer.Tests/ContainerTests.cs
+++ XirzoDIContainer.Tests/ContainerTests.cs
@@ -1,21 +1,18 @@
 ﻿using XirzoDIContainer.Container;
+using XirzoResult;
 
 namespace XirzoDIContainer.Tests
 {
     internal interface IMock
     {
-        void Foo();
     }
 
     internal class Mock : IMock
     {
-        public void Foo()
-        {
-        }
     }
-    
+
     [TestFixture]
-    public class ContainerDiTests
+    internal class ContainerDiTests
     {
         private ContainerDi _container;
 
@@ -24,30 +21,107 @@ namespace XirzoDIContainer.Tests
         {
             _container = new ContainerDi();
         }
+        
+        // ------------------------------------------------------------- Singleton with instance
 
         [Test]
-        public void Bind_SingleClassInstance_ReturnsSameInstance()
+        public void Bind_SingleInstance_ReturnsSameInstance()
         {
             var instance = new Mock();
-            
             _container.Bind<Mock>(instance);
-            var resolved = _container.Resolve<Mock>();
-            
-           Assert.That(instance, Is.EqualTo(resolved), "Instances do not match");
+
+            Result<Mock> resolveResult = _container.Resolve<Mock>();
+
+            Assert.That(resolveResult.Value, Is.EqualTo(instance), "Resolved instance should match original instance");
         }
-        
 
         [Test]
-        public void Bind_SingleClassInstanceTwoTimes_ReturnsError()
+        public void Bind_SingleInstanceToInterface_ReturnsSameInstance()
+        {
+            var instance = new Mock();
+            _container.Bind<IMock>(instance);
+
+            Result<IMock> resolveResult = _container.Resolve<IMock>();
+
+            Assert.That(resolveResult.Value, Is.EqualTo(instance), "Resolved instance should match original instance");
+        }
+        
+        [Test]
+        public void Bind_SingleInstancesMultipleTimes_ReturnsError()
         {
             var instance1 = new Mock();
             var instance2 = new Mock();
-            
-            _container.Bind<Mock>(instance1);
-            _container.Bind<Mock>(instance2);
-            var resolved = _container.Resolve<Mock>();
-            
-           Assert.That(instance1, Is.EqualTo(resolved), "Instances do not match");
+
+            _container.Bind<IMock>(instance1);
+            _container.Bind<IMock>(instance2);
+
+            Result<IMock> resolveResult = _container.Resolve<IMock>();
+
+            Assert.That(resolveResult.Error, Does.Contain("Mock"),
+                "Error should mention the type that couldn't be resolved");
+        }
+        
+        // ------------------------------------------------------------- Singleton without instance
+
+
+        [Test]
+        public void Bind_Singleton_ReturnsSameInstance()
+        {
+            _container.Bind<Mock>().AsSingleton();
+
+            Result<Mock> resolveResult1 = _container.Resolve<Mock>();
+            Result<Mock> resolveResult2 = _container.Resolve<Mock>();
+
+            Assert.That(resolveResult1.Value, Is.EqualTo(resolveResult2.Value), "Resolved instance should match each other");
+        }
+        
+        [Test]
+        public void Bind_SingletonToInterface_ReturnsSameInstance()
+        {
+            _container.Bind<IMock, Mock>().AsSingleton();
+
+            Result<IMock> resolveResult1 = _container.Resolve<IMock>();
+            Result<IMock> resolveResult2 = _container.Resolve<IMock>();
+
+            Assert.That(resolveResult1.Value, Is.EqualTo(resolveResult2.Value), "Resolved instance should match each other");
+        }
+
+
+        // ------------------------------------------------------------- Transient
+        
+        [Test]
+        public void Bind_Transient_ReturnsDifferentInstances()
+        {
+            _container.Bind<Mock>().AsTransient();
+
+            Result<Mock> resolveResult1 = _container.Resolve<Mock>();
+            Result<Mock> resolveResult2 = _container.Resolve<Mock>();
+
+            Assert.That(resolveResult1.Value, Is.Not.EqualTo(resolveResult2.Value), "Resolved instance should not match each other");
+        }
+        
+        [Test]
+        public void Bind_TransientToInterface_ReturnsDifferentInstances()
+        {
+            _container.Bind<IMock, Mock>().AsTransient();
+
+            Result<IMock> resolveResult1 = _container.Resolve<IMock>();
+            Result<IMock> resolveResult2 = _container.Resolve<IMock>();
+
+            Assert.That(resolveResult1.Value, Is.Not.EqualTo(resolveResult2.Value), "Resolved instance should not match each other");
+        }
+        
+        
+        // ------------------------------------------------------------- Non typical errors
+        
+        
+        [Test]
+        public void Resolve_UnregisteredType_ReturnsMeaningfulError()
+        {
+            Result<IMock> resolveResult = _container.Resolve<IMock>();
+
+            Assert.That(resolveResult.Error, Does.Contain("IMock"),
+                "Error should mention the type that couldn't be resolved");
         }
     }
 }
